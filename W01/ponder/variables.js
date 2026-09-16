509c0fc56dee25805abc05ae03b65a7c91292915
byui-Hera <herahysi@gmail.com>
2026-09-16 15:12:33 -0600

    save the changes

---
+++ W01/ponder/variables.js
@@ -0,0 +1,27 @@
+// Declare a variable 
+let age = 22;
+
+// Like print() in Python
+console.log(age);
+
+// A constant variable - can't be changed
+const policyDisclaimer = "Just because you give us money, doesn't mean we have to give it back to you"
+console.log(policyDisclaimer);
+
+const username = "billybob";
+
+// Scope = where you can reference a variable by name
+if (age == 22)
+{
+    // New Scope
+    console.log(username);
+    let favoriteColor = "blue";
+    console.log(favoriteColor);
+    let name = "Dr Java";
+    console.log(name);
+}
+
+// Grab our H1 from the DOM and change its color to blue
+document.querySelector("h1").style.color = "blue";
+
+
